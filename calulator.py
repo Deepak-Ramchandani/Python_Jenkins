c41373d12e6da286accad493d1fdf5c88d148c85
Deepak-Ramchandani <90027919+Deepak-Ramchandani@users.noreply.github.com>
2021-09-13 02:22:11 +0530

    Multiplication and Division operation added

--- calulator.py
+++ calulator.py
@@ -4,3 +4,7 @@ add=a+b
 print("addition is",add)
 subraction=a-b
 print("subtraction is",subtraction)
+multiplication=a*b
+print("multiplication is",multiplication)
+division=a/b
+print(division is",division)
